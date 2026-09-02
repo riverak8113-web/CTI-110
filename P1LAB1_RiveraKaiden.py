c1e96b3679a070286210058458dc515b78d665a9
riverak8113-web <riverak8113@student.faytechcc.edu>
2026-09-02 16:14:43 -0400

    P1LAB1 - First Python Assignment
    Taking Inputs from user, printing output.

---
+++ P1LAB1_RiveraKaiden.py
@@ -0,0 +1,9 @@
+# Kaiden Rivera
+# 09/2/2026
+# P1LAB1
+# The following program prompts the user for their first name, last name, and will print a welcome message welcoming the user. It will store each in its own variable.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, ",first_name,last_name +"!","Welcome to CTI-110!")
